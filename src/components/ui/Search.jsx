60795d4c36bada3cc1a9482717360c6a137362dc
ZAHIN <abuzahinmohammadnowsin@gmail.com>
2025-06-03 23:21:45 +0600

    implement debounced search functionality in Search component

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,28 +1,31 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import searchImg from '../../assets/img/search.svg';
 import { LocationContext } from '../../context';
 import { getLocationByName } from '../../data/locationData';
+import { useDebounce } from '../../hooks';
 
 export default function Search() {
-    const [searchTerm, setSearchTerm] = useState('');
     const { setSelectedLocation } = useContext(LocationContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(`Searching for location: ${searchTerm}`);
-        const fetchedLocation = getLocationByName(searchTerm);
+    const doSearch = useDebounce((term) => {
+        console.log(`Searching for location: ${term}`);
+        const fetchedLocation = getLocationByName(term);
         console.log(fetchedLocation);
         setSelectedLocation({...fetchedLocation });
-    };
+    }, 1000);
+
+    function handleChange (e) {
+        const value = e.target.value;
+        doSearch(value);
+    }
 
     return (
-        <form action="#" onSubmit={handleSubmit}>
+        <form action="#">
             <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-gray-700 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
                 <input
                     className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
                     type="search"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onChange={handleChange}
                     placeholder="Search Location" required
                 />
                 <button type="submit" className="cursor-pointer">
